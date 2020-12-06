67d643741f8dee7b2c4b44e6d160eb7a4961f4e2
sorude26 <sorude26@gmail.com>
2020-12-06 20:37:27 +0900

    EnemyAI調整

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -27,57 +27,7 @@ public class Enemy : Unit
         {
             if (enemyAI == EnemyAI.Attacker)
             {
-                if (!search)
-                {
-                    gameMap.StartSearch(this);
-                    for (int i = 0; i < gameMap.maxX; i++)
-                    {
-                        for (int j = 0; j < gameMap.maxZ; j++)
-                        {
-                            if (gameMap.MoveList[i][j].movePoint > 0)
-                            {
-                                int a = 0;
-                                foreach (Player target in unitManager.GetPlayerList())
-                                {
-                                    int point = 0;
-                                    Vector3 dir = target.transform.position - new Vector3(i * gameMap.mapScale, gameMap.MoveList[i][j].Level, j * gameMap.mapScale);
-                                    if (dir.sqrMagnitude <= DetectionRange * DetectionRange)
-                                    {
-                                        point += (movePower - gameMap.MoveList[i][j].movePoint) * 10;
-                                        point += (target.GetMaxHp() - target.CurrentHp) * 100;
-                                        point -= a;
-                                        if (a != 0)
-                                        {
-                                            if (point > targetUnit.TargetPoint)
-                                            {
-                                                targetUnit = new Target(target, point, i, j);
-                                            }
-                                        }
-                                        else
-                                        {
-                                            targetUnit = new Target(target, point, i, j);
-                                        }
-                                    }
-                                    a++;
-                                }
-                            }
-                        }
-                    }
-                    search = true;
-                }
-                if (targetUnit != null)
-                {
-                    if (!move)
-                    {
-                        UnitMove(gameMap.MoveList, targetUnit.PosX, targetUnit.PosZ);
-                        move = true;
-                    }
-                }
-                else
-                {
-                    search = false;
-                    ActionNow = false;
-                }
+                ActionTypeAttacker();
             }
             if (moveMood)
             {
@@ -96,9 +46,9 @@ public class Enemy : Unit
                 attack = true;
             }
             UnitAngleControl();
-            if (attack)
+            if (attack)//攻撃指示実行後ターゲット含めリセットし行動終了
             {
-                if (targetUnit != null)
+                if (targetUnit != null)//ターゲットが存在する場合に攻撃
                 {
                     TargetShot(targetUnit.TargetUnit);
                 }
@@ -114,5 +64,60 @@ public class Enemy : Unit
     public void StatAction()
     {
         ActionNow = true;
+    }
+
+    private void ActionTypeAttacker()
+    {
+        if (!search)
+        {
+            gameMap.StartSearch(this);
+            for (int i = 0; i < gameMap.maxX; i++)
+            {
+                for (int j = 0; j < gameMap.maxZ; j++)
+                {
+                    if (gameMap.MoveList[i][j].movePoint > 0)
+                    {
+                        int number = 0;
+                        foreach (Player target in unitManager.GetPlayerList())//ユニットが移動後の索敵範囲にいるか検索
+                        {
+                            int point = 0;
+                            Vector3 dir = target.transform.position - new Vector3(i * gameMap.mapScale, gameMap.MoveList[i][j].Level, j * gameMap.mapScale);
+                            if (dir.sqrMagnitude <= DetectionRange * DetectionRange)
+                            {
+                                point += (movePower - gameMap.MoveList[i][j].movePoint) * 10;//移動量が多い場合に高得点
+                                point += (target.GetMaxHp() - target.CurrentHp) * 100;//ターゲットの耐久値の減少量が大きい場合に高得点
+                                point -= number;//ターゲットの登録順で得点に差
+                                if (targetUnit != null)//ターゲットが登録済みか判断し、登録済みのターゲットポイントと比較、高ポイントならば新規登録
+                                {
+                                    if (point > targetUnit.TargetPoint)
+                                    {
+                                        targetUnit = new Target(target, point, i, j);
+                                    }
+                                }
+                                else
+                                {
+                                    targetUnit = new Target(target, point, i, j);
+                                }
+                            }
+                            number++;
+                        }
+                    }
+                }
+            }
+            search = true;
+        }
+        if (targetUnit != null)//ターゲットが設定されているならば移動実施
+        {
+            if (!move)
+            {
+                UnitMove(gameMap.MoveList, targetUnit.PosX, targetUnit.PosZ);
+                move = true;
+            }
+        }
+        else
+        {
+            search = false;
+            ActionNow = false;
+        }
     }
 }
