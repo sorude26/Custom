bdb670f52794ea4a35d8efc6a821ea0bd1dd7dff
sorude26 <sorude26@gmail.com>
2020-12-08 18:02:57 +0900

    パーツリスト制作

---
+++ Assets/Scripts/Parts/UnitPartsList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitPartsList : MonoBehaviour
+{
+    [SerializeField]
+    List<PartsHead> HeadList;
+    [SerializeField]
+    List<PartsBody> BodyList;
+    [SerializeField]
+    List<PartsLArm> LArmsList;
+    [SerializeField]
+    List<PartsRArm> RArmsList;
+    [SerializeField]
+    List<PartsLeg> LegList;
+
+    public PartsHead GetPartsHead(int i) { return HeadList[i]; }
+    public PartsBody GetPartsBody(int i) { return BodyList[i]; }
+    public PartsLArm GetPartsLArm(int i) { return LArmsList[i]; }
+    public PartsRArm GetPartsRArm(int i) { return RArmsList[i]; }
+    public PartsLeg GetPartsLeg(int i) { return LegList[i]; }
+}
