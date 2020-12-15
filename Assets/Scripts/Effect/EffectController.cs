be39c9a43fb7fe172265af46203787beb9ce2f04
sorude26 <sorude26@gmail.com>
2020-12-15 17:04:08 +0900

    スクリプトファイル整理、エフェクト追加

---
+++ Assets/Scripts/Effect/EffectController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectController : MonoBehaviour
+{
+	ParticleSystem[] particleSystems = null;
+
+	private void Awake()
+	{
+		// PlayOnAwakeがTRUEの場合、再生されるので、停止させる。
+		particleSystems = GetComponentsInChildren<ParticleSystem>();
+		Stop();
+
+		// 最初は非アクティブにしておく
+		gameObject.SetActive(false);
+	}
+
+	private void Update()
+	{
+		foreach (var particle in particleSystems)
+		{
+			if (particle.isPlaying)
+			{
+				return;
+			}
+		}
+
+		gameObject.SetActive(false);
+	}
+
+	public void Play(Vector3 pos)
+	{
+		gameObject.SetActive(true);
+		transform.localPosition = pos;
+
+		foreach (var particle in particleSystems)
+		{
+			particle.Play();
+		}
+	}
+
+	public void Stop()
+	{
+		foreach (var particle in particleSystems)
+		{
+			particle.Stop();
+		}
+	}
+
+	public bool IsActive()
+	{
+		return gameObject.activeInHierarchy;
+	}
+
+	public Transform GetTransform()
+	{
+		return transform;
+	}
+}
---
+++ Assets/Scripts/Effect/EffectManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EffectID
+{
+	None,   // 未定義
+
+	Explosion,  // 爆発
+	Hit,
+	// 下に追加する
+	// 途中で追加しない！
+}
+
+public class EffectManager : MonoBehaviour
+{
+	[System.Serializable]
+	class EffectData
+	{
+		public GameObject effectPrefab = null;
+		public int createCount = 0;
+	}
+
+	[SerializeField]
+	List<EffectData> effectList = null;
+
+	Dictionary<EffectID, List<EffectController>> effectPoolList = new Dictionary<EffectID, List<EffectController>>();
+
+	static EffectManager instance = null;
+
+	private void Awake()
+	{
+		instance = this;
+
+		GameObject root = new GameObject("EffectRoot");
+
+		for (int i = 0; i < effectList.Count; i++)
+		{
+			var effectID = (EffectID)(i + 1);
+			effectPoolList.Add(effectID, new List<EffectController>());
+
+			for (int k = 0; k < effectList[i].createCount; k++)
+			{
+				var instance = Instantiate(effectList[i].effectPrefab, root.transform);
+				var effectCtrl = instance.AddComponent<EffectController>();
+				effectPoolList[effectID].Add(effectCtrl);
+			}
+		}
+	}
+
+	public static EffectController PlayEffect(EffectID effectID, Vector3 pos)
+	{
+		foreach (var effect in instance.effectPoolList[effectID])
+		{
+			if (effect.IsActive())
+			{
+				continue;
+			}
+
+			effect.Play(pos);
+			return effect;
+		}
+
+		return null;
+	}
+}
+
