78747aeeb3ea42d1d878e6e0c6d7ececfb15b00d
sorude26 <sorude26@gmail.com>
2020-12-21 14:27:14 +0900

    ステージデータスクリプト追加

---
+++ Assets/Scripts/Stage/StageData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum StageType
+{
+    City1,
+    Forest1,
+    Wasteland,
+}
+public class StageData : MonoBehaviour
+{
+    public float Level { get;private set; }
+    public Map.MapType MapTypeData { get; private set; }
+
+    void StageDataGet()
+    {
+
+    }
+}
