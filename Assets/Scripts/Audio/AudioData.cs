76d7291abce970f8b2b154d3afe7050a2360a7b1
sorude26 <sorude26@gmail.com>
2021-02-01 14:25:34 +0900

    サウンド系仮作成

---
+++ Assets/Scripts/Audio/AudioData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AudioType
+{
+
+}
+public enum SEType
+{
+
+}
+public class AudioData : MonoBehaviour
+{
+    [SerializeField] AudioClip[] BGMdata;
+    [SerializeField] AudioClip[] SEdata;
+    public static AudioData Instance { get; private set; }
+
+    private void Start()
+    {
+        Instance = this;
+    }
+     
+    public AudioClip GetBGM(int i) 
+    {
+        return BGMdata[i];
+    }
+    public AudioClip GetSE(int i)
+    {
+        return BGMdata[i];
+    }
+}
---
+++ Assets/Scripts/Audio/AudioObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioObject : MonoBehaviour
+{
+    AudioSource audioSource;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(AudioClip audio)
+    {
+        audioSource.PlayOneShot(audio);
+    }
+}
