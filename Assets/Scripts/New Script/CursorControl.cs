7a1b4a3e1f1acdc772d248d11fb3f0fc15fe39c5
sorude26 <sorude26@gmail.com>
2021-05-17 22:50:52 +0900

    カーソル操作用スクリプトを追加した

---
+++ Assets/Scripts/New Script/CursorControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorControl : MonoBehaviour
+{
+    int cursorPosX;
+    int cursorPosZ;
+    bool cursorControl;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (!cursorControl)
+        {
+            return;
+        }
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            float h = Input.GetAxisRaw("Horizontal");
+            if (h > 0)
+            {
+                cursorPosX++;
+            }
+            else if(h < 0)
+            {
+                cursorPosX--;
+            }
+        }
+        if (Input.GetButtonDown("Vertical"))
+        {
+            float v = Input.GetAxisRaw("Vertical");
+            if (v > 0)
+            {
+                cursorPosZ++;
+            }
+            else if(v < 0)
+            {
+                cursorPosZ--;
+            }
+        }
+    }
+}
